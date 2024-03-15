19f0a111fc19a727f652f56495b8014aa20f4f26
Anthony <antho.2999@gmail.com>
2024-03-15 18:01:20 -0400

    first commit

---
+++ src/textnode.py
@@ -0,0 +1,13 @@
+class TextNode:
+    def __init__(self, text, text_type, url=None) -> None:
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+        return
+    def __eq__(self, other) -> bool:
+        text_props_equal = self.text == other.text
+        text_type_props_equal = self.text_type == other.text_type
+        url_props_equal = self.url == other.url
+        return text_props_equal and text_type_props_equal and url_props_equal
+    def __repr__(self) -> str:
+        return f"TextNode({self.text},{self.text_type},{self.url})"
