0e3f63e84e17b6adfe9095e172d959404c788960
Anthony <antho.2999@gmail.com>
2024-03-18 20:45:14 -0400

    Added node that represents unformatted text

--- src/textnode.py
+++ src/textnode.py
@@ -4,10 +4,12 @@ class TextNode:
         self.text_type = text_type
         self.url = url
         return
+
     def __eq__(self, other) -> bool:
         text_props_equal = self.text == other.text
         text_type_props_equal = self.text_type == other.text_type
         url_props_equal = self.url == other.url
         return text_props_equal and text_type_props_equal and url_props_equal
+
     def __repr__(self) -> str:
         return f"TextNode({self.text},{self.text_type},{self.url})"
